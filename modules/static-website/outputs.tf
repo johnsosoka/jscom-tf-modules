d877009c7b0454d9a406393acbe02735dc8f3edd
John Sosoka <john@sosoka.com>
2025-10-28 17:47:37 -0600

    feat: add cloud front ID to website outputs

--- modules/static-website/outputs.tf
+++ modules/static-website/outputs.tf
@@ -2,3 +2,8 @@ output "website_bucket_id" {
   description = "The ID of the bucket from the static website module"
   value       = aws_s3_bucket.bucket.id
 }
+
+output "cloudfront_distribution_id" {
+  description = "The ID of the related CloudFront distribution in-front of S3"
+  value = aws_cloudfront_distribution.website_distribution.id
+}
